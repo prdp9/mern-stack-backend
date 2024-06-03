559df9464884cbf165904e580c567ad9447fb182
prdp99 <iampradippariyar@gmail.com>
2024-06-03 21:22:59 +0545

    modified frontend cors url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-	origin: "http://localhost:5173",
+	origin: process.env.FRONTEND_URL || "http://localhost:5173",
 	credentials: true
 }))
 
@@ -26,7 +26,7 @@ app.use('/uploads',express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 app.use(cookieParser())
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req,res) => {
 res.send('server is running :)')
